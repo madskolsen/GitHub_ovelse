2c702295eec844f57183ba6f38c22f48d86218e3
madskolsen <89842092+madskolsen@users.noreply.github.com>
2022-02-21 09:40:56 +0100

    Existensiel crisis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,7 @@ public class Main {
         System.out.println("Hello world!2");
         System.out.println("Hello world!3");
         System.out.println("Hello world!4");
-        System.out.println("Hello world!5");    }
+        System.out.println("Hello world!5");
+        System.out.println("Why do i exist?");
+    }
 }
